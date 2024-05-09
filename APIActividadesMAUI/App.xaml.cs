286ab564b23e731ee9779be0fb04426a42c1ebed
Rodrigo Elias Castillo <201G0246@rcarbonifera.tecnm.mx>
2024-05-08 22:54:24 -0600

    Agregar archivos de proyecto.

---
+++ APIActividadesMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace APIActividadesMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ APIActividadesMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace APIActividadesMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
