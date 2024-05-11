ff8db25cc1f3a10f70be1ce5dbdb92548b38b225
Rodrigo Elias Castillo <201G0246@rcarbonifera.tecnm.mx>
2024-05-10 19:42:27 -0600

    avances validator

---
+++ APIActividadesITESRC/Models/Validators/DepartamentoValidator.cs
@@ -0,0 +1,13 @@
+﻿using APIActividadesITESRC.Models.DTOs;
+using FluentValidation;
+
+namespace APIActividadesITESRC.Models.Validators
+{
+    public class DepartamentoValidator:AbstractValidator<DepartamentoDTO>
+    {
+        public DepartamentoValidator()
+        {
+            RuleFor(x => x.Nombre).NotEmpty().WithMessage("El nombre del departamento no debe estar vacío.");
+        }
+    }
+}
