7bbbe20c8f218d4b136ca255510169e6bd5809e4
Rodrigo Elias Castillo <201G0246@rcarbonifera.tecnm.mx>
2024-05-12 21:37:54 -0600

    algunos cambios

---
+++ APIActividadesITESRC/Helper/GeneradorToken.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace APIActividadesITESRC.Helper
+{
+    public class GeneradorToken
+    {
+        public string GetToken(string token)
+        {
+            List<Claim> claims = new();
+            claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+
+        }
+    }
+}
