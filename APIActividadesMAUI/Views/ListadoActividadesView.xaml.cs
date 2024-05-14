42e87a78a4dce8f09816edea2edcabeb97df7ea8
Abraham Torres <abraham.torres@r2devpros.com>
2024-05-14 16:48:56 -0600

    Vistas base

--- APIActividadesMAUI/Views/ListadoActividadesView.xaml.cs
+++ APIActividadesMAUI/Views/ListadoActividadesView.xaml.cs
@@ -1,9 +1,16 @@
+using APIActividadesMAUI.Views.ActividadesView;
+
 namespace APIActividadesMAUI.Views;
 
 public partial class ListadoActividadesView : ContentPage
 {
 	public ListadoActividadesView()
 	{
-		InitializeComponent();
+		
 	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		await Navigation.PushAsync(new AgregarActividadView());
+    }
 }
