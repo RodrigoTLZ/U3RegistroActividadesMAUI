e01d7de63ff8d5e213a18907d27bf94b5edb1c0b
RodrigoTLZ <201G0246@rcarbonifera.tecnm.mx>
2024-06-09 16:16:15 -0600

    actualizacion cadena de conexion BD

--- APIActividadesITESRC/Program.cs
+++ APIActividadesITESRC/Program.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ItesrcneActividadesContext>(x => x.UseMySql("server=204.93.216.11;database=itesrcne_actividades;user=itesrcne_deptos;password=sistemaregistrotec24", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.3.29-mariadb")));
+builder.Services.AddDbContext<ItesrcneActividadesContext>(x => x.UseMySql("server=187.216.2.135;database=itesrcne_actividades;user=itesrcne_deptos;password=sistemaregistrotec24", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.3.29-mariadb")));
 builder.Services.AddTransient<DepartamentosRepository>();
 builder.Services.AddTransient<ActividadesRepository>();
 var connectionString = builder.Configuration.GetConnectionString("ActividadesConnectionString");
